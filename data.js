cdffc5fd62e1f3772e96eea64c0c8ef7dbb26eb9
SAHIBJOT SINGH BENIPAL <sahibjot.benipal2003@gmail.com>
2024-06-30 14:07:19 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,184 @@
+let songs =[
+
+    {
+        name: '295',
+        path: './songs/295 - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/size_m_1621144374 295.webp'
+    },
+    {
+        name: 'Drippy',
+        path: './songs/Drippy - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/artworks-QKrtMOYHGQSkBC1N-pyT8Yw-t500x500 dripy.webp'
+    },
+    {
+        name: 'Burberry',
+        path: './songs/Burberry - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/artworks-kD468on3EXZGPnwm-kiu74g-t500x500 burr.webp'
+    },
+    {
+        name: 'Watch Out',
+        path: './songs/Watch Out - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/Watch Out.webp'
+    },
+    {
+        name: 'Invincible',
+        path: './songs/Invincible - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/artworks-1o9yIfHsizHzsvor-o10soA-t500x500 invinc.webp'
+    },
+    {
+        name: 'Punjab',
+        path: './songs/Panjab (My Motherland) - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/Panjab-1.webp'
+    },
+    {
+        name: '911',
+        path: './songs/911 - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/911-1.webp'
+    },
+    {
+        name: 'Dhakka',
+        path: './songs/Dhakka (Original) - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/Dhakka.webp'
+    },
+    {
+        name: 'Satisfy',
+        path: './songs/Satisfy - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/Satisfy-1.webp'
+    },
+    {
+        name: 'Jailaan',
+        path: './songs/Jailaan (Moosa Jatt) - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/Jailaan-From-Moosa-Jatt-1.webp'
+    },
+    {
+        name: 'Calaboose',
+        path: './songs/Calaboose - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/cala.webp'
+    },
+    {
+        name: 'B & W',
+        path: './songs/B & W - DjPunjab.Com.Se.mp3',
+        artist: 'Sidhu Moosewala',
+        img: './images/BW.webp'
+    },
+    {
+        name: 'King Shit',
+        path: './songs/King Shit.mp3',
+        artist: 'Shubh',
+         img: './images/king.webp'
+    },
+    {
+        name: 'Still Rollin',
+        path: './songs/Still Rollin - Shubh.mp3',
+        artist: 'Shubh',
+         img: './images/still.webp'
+    },
+    {
+        name: 'No Love',
+        path: './songs/No Love - Shubh.mp3',
+        artist: 'Shubh',
+         img: './images/No-Love-mp3-image-300x300.webp'
+    },
+    {
+        name: 'Happy beats',
+        path: './songs/1.mp3',
+        artist: 'Artist Unknown',
+        img: './images/1.webp'
+    },
+    {
+        name: 'Sunrise Bliss',
+        path: './songs/2.mp3',
+        artist: 'Artist Unknown',
+        img: './images/2.webp'
+    },
+    {
+        name: 'Good Day',
+        path: './songs/4.mp3',
+        artist: 'Artist Unknown',
+        img: './images/3.webp'
+    },
+    {
+        name: 'Titanium',
+        path: './songs/titanium-170190.mp3',
+        artist: 'Artist Unknown',
+         img: './images/4.webp'
+    },
+    {
+        name: 'Epic Short',
+        path: './songs/5.mp3',
+        artist: 'Artist Unknown',
+         img: './images/5.webp'
+    },
+    {
+        name: 'Cheques',
+        path: './songs/Cheques - Shubh.mp3',
+        artist: 'Shubh',
+         img: './images/11.webp'
+    },
+    {
+        name: 'Challa',
+        path: './songs/Challa - Sidhu Moose Wala.mp3',
+        artist: 'Sidhu Moosewala',
+         img: './images/12.webp'
+    },
+    {
+        name: 'Top Class',
+        path: './songs/Top Class Overseas - Karan Aujla.mp3',
+        artist: 'Karan Aujla',
+         img: './images/13.webp'
+    },
+    {
+        name: 'Punjab',
+        path: './songs/Punjab Intro - Arjan Dhillon.mp3',
+        artist: 'Arjan Dillon',
+         img: './images/14.jpeg'
+    },
+    {
+        name: 'Countrysider',
+        path: './songs/Countrysiders.mp3',
+        artist: 'Tarsem Jassar',
+         img: './images/j1.webp'
+    },
+    
+    {
+        name: 'Ballar',
+        path: './songs/Baller - Shubh.mp3',
+        artist: 'Shubh',
+        img: './images/ballar.webp'
+    },
+    {
+        name: 'Ice',
+        path: './songs/Ice - Shubh.mp3',
+        artist: 'Shubh',
+         img: './images/ice.webp'
+    },
+    {
+        name: 'Safety Off',
+        path: './songs/Safety Off - Shubh.mp3',
+        artist: 'Shubh',
+         img: './images/safety.webp'
+    },
+    {
+        name: 'Ruthless',
+        path: './songs/Ruthless - Shubh.mp3',
+        artist: 'Shubh',
+         img: './images/ruth.webp'
+    },
+    {
+        name: 'Hood Anthem',
+        path: './songs/Hood Anthem - Shubh.mp3',
+        artist: 'Shubh',
+         img: './images/hood.webp'
+    }
+  ]
